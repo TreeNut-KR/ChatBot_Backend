d7f759afcce466ea0cf4ba06d322174e9c54e0f6
Cut the Wire <102637600+CaDiVae@users.noreply.github.com>
2024-07-26 18:41:55 +0900

    spring boot 초기 구성

---
+++ TreeNut/src/main/java/com/example/TreeNut/TreeNutApplication.java
@@ -0,0 +1,13 @@
+package com.example.TreeNut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeNutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeNutApplication.class, args);
+	}
+
+}
---
+++ TreeNut/src/main/java/com/example/TreeNut/WebController.java
@@ -0,0 +1,13 @@
+package com.example.TreeNut;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // templates/index.html을 반환
+    }
+}
